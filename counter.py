7715f173a7cfe8c4cbc9a7b25d4e63830bce0a57
Iskander Shakirov <is2367361@gmail.com>
2022-12-12 13:35:38 +0300

    Made some changes

--- counter.py
+++ counter.py
@@ -1,38 +1,37 @@
-f = open("Obchak.txt").readline
-n = int(f())
-names = f().split()
-d = {}
-for i in names:
-    d[i] = 0
-for i in range(n):
-    s = f().split()
-    # if '\n' in s[-1]: s[-1] = s[-1][:-1]
-    for i in range(len(s)):
-        if i == 1: s[1] = int(s[1])
-        else: s[i] = s[i].lower()
-    summ = s[1] / (len(names) - (len(s) - 2))
-    d[s[0]] -= s[1]
-    if s[0] not in s[2:]: d[s[0]] += summ
-    for j in names:
-        if j not in s:
-            d[j] += summ
-pos_d = {}
-neg_d = {}
-for key, value in d.items():
-    if value > 0: pos_d[key] = value
-    else: neg_d[key] = value
-pos_d = dict(sorted(pos_d.items(), key=lambda item: item[1], reverse=True))
-neg_d = dict(sorted(neg_d.items(), key=lambda item: item[1]))
-for key, value in pos_d.items():
-    for k, v in neg_d.items():
-        if v == 0: continue
-        if pos_d[key] + neg_d[k] <= 0:
-            print(f'{key} should pay to {k} ===>>> {pos_d[key]}')
-            neg_d[k] += pos_d[key]
-            pos_d[key] = 0
-            break
-        else:
-            print(f'{key} should pay to {k} ===>>> {-neg_d[k]}')
-            pos_d[key] += neg_d[k]
-            neg_d[k] = 0
-input()
+def solve(d):
+    ans = "```\n"
+    pos_d = {}
+    neg_d = {}
+    for key, value in d.items():
+        if value > 0: pos_d[key] = value
+        else: neg_d[key] = value
+    pos_d = dict(sorted(pos_d.items(), key=lambda item: item[1], reverse=True))
+    neg_d = dict(sorted(neg_d.items(), key=lambda item: item[1]))
+    for key, value in pos_d.items():
+        for k, v in neg_d.items():
+            if v == 0: continue
+            if pos_d[key] + neg_d[k] <= 0:
+                ans += f"{key} should pay to {k} ===>>> {pos_d[key]}\n"
+                neg_d[k] += pos_d[key]
+                pos_d[key] = 0
+                break
+            else:
+                ans += f'{key} should pay to {k} ===>>> {-neg_d[k]}\n'
+                pos_d[key] += neg_d[k]
+                neg_d[k] = 0
+    ans += '```'
+    return ans
+
+
+def count(arr, names):
+    d = {}
+    for i in names:
+        d[i] = 0
+    for mas in arr:
+        mas = mas[2:]
+        summ = mas[1] / (len(names) - (len(mas[2])))
+        d[mas[0]] -= mas[1]
+        for j in names:
+            if j not in mas[2]:
+                d[j] += summ
+    return(solve(d))
--- obchak.py
+++ obchak.py
@@ -2,6 +2,7 @@ import telebot
 from telebot import types
 from prettytable import PrettyTable
 import json
+import counter
 
 # token = "5539695234:AAELsA-kz78QW6XWLR0dLSkPR1XSHzdvgJo"
 token = "5094695265:AAHl3W5t1QVl8F3BlJS4S7usojzZKxnQOWQ"
@@ -11,6 +12,7 @@ date = ''
 for_what = ""
 who = ''
 how_much = 0
+cnt = 0
 without_whom = []
 users = ['isk', 'doc', 'maj', 'sen', 'dep', 'jos']
 
@@ -29,7 +31,8 @@ def start(message):
         btn2 = types.KeyboardButton("show")
         btn3 = types.KeyboardButton("change")
         btn4 = types.KeyboardButton("delete")
-        markup.add(btn1, btn2, btn3, btn4)
+        btn5 = types.KeyboardButton("count")
+        markup.add(btn1, btn2, btn3, btn4, btn5)
         bot.send_message(message.chat.id,
                          text="Выбери нужную тебе команду".format(
                              message.from_user), reply_markup=markup)
@@ -42,12 +45,25 @@ def show_data(id):  # Printing all data about obchak
     mytable.field_names = ['Date', "For what", "Who", "How much", "Without whom"]
     f = open('data.txt').readline
     arr = []
-    for i in range(3):
+    for i in range(cnt):
         arr.append(json.loads(f()[:-1]))
     mytable.add_rows(arr)
     mytable.align = 'c'
     s = "```\n" + str(mytable) + "\n```"
     bot.send_message(id, text=s, parse_mode="Markdown")
+
+
+def count(id):
+    if cnt == 0:
+        bot.send_message(id, text="Никто никому ничего не должен, классно же)")
+        return
+
+    f = open('data.txt').readline
+    arr = []
+    for i in range(cnt):
+        arr.append(json.loads(f()[:-1]))
+    txt = counter.count(arr, users)
+    bot.send_message(id, text=txt, parse_mode="Markdown")
 
 
 @bot.message_handler(content_types=['text'])
@@ -61,18 +77,20 @@ def func(message):
         pass
     elif message.text == "delete":
         pass
+    elif message.text == 'count':
+        count(message.chat.id)
 
 
 def get_date(message):
     global date
-    date = message.text
+    date = message.text.lower()
     bot.send_message(message.chat.id, text="на что закупился? например завтрак, туалетка...")
     bot.register_next_step_handler(message, get_for_what)
 
 
 def get_for_what(message):
     global for_what
-    for_what = message.text
+    for_what = message.text.lower()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("isk")
     btn2 = types.KeyboardButton("doc")
@@ -87,7 +105,7 @@ def get_for_what(message):
 
 def get_who(message):  # передает на что потратили, записать в таблицу
     global who
-    who = message.text
+    who = message.text.lower()
     bot.send_message(message.chat.id, text="Сколько рублей потратил?", reply_markup=types.ReplyKeyboardRemove())
     bot.register_next_step_handler(message, get_how_much)
 
@@ -110,18 +128,27 @@ def get_without_whom(message):
     flag = 1
     if message.text != 'all':
         without_whom = message.text.split()
-        for elem in without_whom:
-            if elem not in users:
+        for i in range(len(without_whom)):
+            without_whom[i] = without_whom[i].lower()
+            if without_whom[i] not in users:
                 flag = 0
                 bot.send_message(message.chat.id,
-                                 text=f"Пользователя {elem} нет в данных, вы уверены, что ввели правильно его ник."
-                                      f"Если да, то сообщите об ошибке @L1km0 или @iskansosik")
+                                 text=f"Пользователя {without_whom[i]} нет в данных, вы уверены, что ввели правильно его ник."
+                                      f"Если да, то сообщите об ошибке @iskansosik")
                 bot.register_next_step_handler(message, get_without_whom)
     if flag:
+        global cnt
+        cnt += 1
         file = open('data.txt', 'a')
         arr = [date, for_what, who, how_much, without_whom]
         json.dump(arr, file)
         file.write("\n")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("show")
+        btn2 = types.KeyboardButton("/start")
+        btn3 = types.KeyboardButton("/obch")
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, text="Добавил данные в таблицу, если хотите проверить корректность нажмите клавишу show", reply_markup=markup)
 
 
 bot.polling(none_stop=True)
